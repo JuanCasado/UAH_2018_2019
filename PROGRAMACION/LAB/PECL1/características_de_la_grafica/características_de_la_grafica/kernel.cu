ae7c310743ec3c79a48c3cc728fb940caa6bc942
JuanCasado <juan.casado@edu.uah.es>
2019-02-05 23:30:30 +0100

    PECL1

--- PROGRAMACION/LAB/PECL1/características_de_la_grafica/características_de_la_grafica/kernel.cu
+++ PROGRAMACION/LAB/PECL1/características_de_la_grafica/características_de_la_grafica/kernel.cu
@@ -3,24 +3,48 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
+/*
+PASOS SEGUIDOS PARA AÑADIR EL ARCHIVO DE CABECERAS: #include "common/book.h"
+	- Clonar el repositorio de GitHub: https://github.com/CodedK/CUDA-by-Example-source-code-for-the-book-s-examples-
+	- Ir a Proyect>Properties en VS
+	- Añadir la raiz del repositorio clonado a: INCLUDE PATH
+	- Añadir el directorio lib del repositorio clonado a: LIBRARY PATH
+	- Añadir el directorio bin del repositorio clonado a: EXECUTABLE PATH
+*/
+#include "common/book.h"
+
 int main() {
 	int nDevices;
-	cudaGetDeviceCount(&nDevices);
+	HANDLE_ERROR(cudaGetDeviceCount(&nDevices));
 	for (int i = 0; i < nDevices; i++) {
 		cudaDeviceProp prop;
-		cudaGetDeviceProperties(&prop, i);
+		HANDLE_ERROR(cudaGetDeviceProperties(&prop, i));
 		printf("\n------------ Device Number: %d ------------\n\n", i);
-		printf("  Device name: %s\n", prop.name);
+		printf("Device name: %s\n\n", prop.name);
+		printf("Calculo como en el ejercicio de clase:\n");
 		printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
 		printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
 		printf("  Peak Memory Bandwidth (GB/s): %f\n\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth / 8) / 1.0e6);
+
+		printf("Total global memory %lld\n", prop.totalGlobalMem);
+		printf("Total const memory %d\n", prop.totalConstMem);
+		printf("Memory pitch %d\n\n", prop.memPitch);
 		
+		printf("Multiprocesor count %d\n", prop.multiProcessorCount);
+		printf("Max Threads per multiprocesor %d\n", prop.maxThreadsPerMultiProcessor);
+		printf("Shared memory per multiprocesor %d\n", prop.sharedMemPerMultiprocessor);
+		printf("Registers per multiprocesor %d\n\n", prop.regsPerMultiprocessor);
+
 		printf("Max Threads per block %d\n", prop.maxThreadsPerBlock);
-		printf("Shared memory per block %d\n\n", prop.sharedMemPerBlock);
+		printf("Shared memory per block %d\n", prop.sharedMemPerBlock);
+		printf("Registers per block %d\n", prop.regsPerBlock);
 		printf("Max block size: ");
-		printf("(\n  [x->%d], \n  [y->%d], \n  [z->%d]\n)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+		printf("( [x->%d], [y->%d], [z->%d] )\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
 		printf("Max grid size: ");
-		printf("(\n  [x->%d], \n  [y->%d], \n  [z->%d]\n)\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+		printf("( [x->%d], [y->%d], [z->%d] )\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+
+		printf("Device Overlap %d\n", prop.deviceOverlap);
+		printf("Kernel Execution Timeout %d\n", prop.kernelExecTimeoutEnabled);
 	}
 	getchar();
     return 0;
--- PROGRAMACION/LAB/PECL1/sumar_tres_enteros/sumar_tres_enteros/kernel.cu
+++ PROGRAMACION/LAB/PECL1/sumar_tres_enteros/sumar_tres_enteros/kernel.cu
@@ -2,6 +2,8 @@
 #include <cuda_runtime.h>
 
 #include <stdio.h>
+#include <ctime>
+#include <cstdlib>
 
 /*
 Adds three templated classes, they must implement the + operator.
@@ -13,9 +15,10 @@ __global__ void add3 (T *result, const T *a, const T *b, const T *c) {
 }
 
 int main() {
-	int ha = 1;
-	int hb = 2;
-	int hc = 3;
+	std::srand(static_cast<int>(time(0)));
+	int ha = static_cast<int>(std::rand() % 100 + 1);;
+	int hb = static_cast<int>(std::rand() % 100 + 1);;
+	int hc = static_cast<int>(std::rand() % 100 + 1);;
 	int *da;
 	int *db;
 	int *dc;
@@ -39,6 +42,12 @@ int main() {
 
 	//CPU <-- GPU
 	cudaMemcpy(&hresult, dresult, sizeof(int), cudaMemcpyDeviceToHost);
+
+	//Free GPU memory
+	cudaFree(da);
+	cudaFree(db);
+	cudaFree(dc);
+	cudaFree(dresult);
 
 	//Show result
 	printf("%d + %d + %d = %d\n", ha, hb, hc, hresult);
